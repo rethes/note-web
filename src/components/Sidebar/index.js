d467ce6def1821df04a9f5633e32be13a4adcfb0
rethes <rethesmutua@gmail.com>
2019-06-25 18:54:09 +0300

    feat(notes): Add a DashboardPage
    - add a DashboardPage component
    - add styling to the DashboardPage
    
    [Finishes #166882327]

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,31 +3,35 @@ import React from "react";
 
 // third-party libraries
 import { Menu, Divider } from 'semantic-ui-react';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 // styles
 import './SideBar.scss';
 
 // components
-import { SideBarItem } from '../SideBarItem';
+import {SideBarItem} from '../SideBarItem';
 
 class SideBar extends React.Component {
   render() {
     return (
       <Menu borderless vertical stackable fixed='left' className='side-nav'>
         <Link to="/" replace>
-        <SideBarItem highlight={true}  label='Home' icon='home'/>
+          <SideBarItem highlight={true} label='Home' icon='home'/>
         </Link>
         <Divider/>
         <SideBarItem label='User Name' icon='user circle'/>
         <Divider/>
         <Link to="/notes/new" replace>
-        <SideBarItem label='New Note' icon='add'/>
+          <SideBarItem label='New Note' icon='add'/>
         </Link>
         <Divider/>
-        <SideBarItem label='All Notes' icon='sticky note'/>
+        <Link to="/notes" replace>
+          <SideBarItem label='All Notes' icon='sticky note'/>
+        </Link>
         <Divider/>
-        <SideBarItem label='Notebooks' icon='folder'/>
+        <Link to="/notebooks" replace>
+          <SideBarItem label='Notebooks' icon='folder'/>
+        </Link>
         <Divider/>
       </Menu>
     );
---
+++ src/pages/NotesListPage/index.js
@@ -0,0 +1,19 @@
+// react libraries
+import React from "react";
+
+// third-party libraries
+
+// styles
+import './NotesListPage.scss';
+
+class NotesListPage extends React.Component {
+
+  render() {
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+export default NotesListPage;
